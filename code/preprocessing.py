8a8c68a51bcd460ff8c56c50912e4ecd26d336ba
Lan <lngoquy1@swarthmore.edu>
2017-10-19 16:28:15 -0400

    code

---
+++ code/preprocessing.py
@@ -0,0 +1,382 @@
+import sys
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import cv2
+import math
+from sklearn import mixture
+from sklearn.utils import shuffle
+from skimage import measure
+from glob import glob
+import seaborn as sns
+import os
+from numpy import sqrt, histogram
+np.random.seed(123)
+from subprocess import check_output
+print(check_output(["ls", "input"]).decode("utf8"))
+
+TRAIN_DATA = "input/train"
+TEST_DATA = "input/test"
+type_1_files = glob(os.path.join(TRAIN_DATA, "Type_1", "*.jpg"))
+type_1_ids = np.array([s[len(os.path.join(TRAIN_DATA, "Type_1"))+1:-4] for s in type_1_files])
+type_2_files = glob(os.path.join(TRAIN_DATA, "Type_2", "*.jpg"))
+type_2_ids = np.array([s[len(os.path.join(TRAIN_DATA, "Type_2"))+1:-4] for s in type_2_files])
+type_3_files = glob(os.path.join(TRAIN_DATA, "Type_3", "*.jpg"))
+type_3_ids = np.array([s[len(os.path.join(TRAIN_DATA, "Type_3"))+1:-4] for s in type_3_files])
+test_files = glob(os.path.join(TEST_DATA, "*.jpg"))
+test_ids = np.array([s[11:-4] for s in test_files])
+print "test ids", len(test_ids), test_ids[:3]
+print "type 1", len(type_1_ids)
+print "type 2", len(type_2_ids)
+print "type 3", len(type_3_ids)
+print "test", len(test_ids)
+def get_filename(image_id, image_type):
+    """
+    Method to get image file path from its id and type
+    """
+    if image_type == "Type_1" or \
+        image_type == "Type_2" or \
+        image_type == "Type_3":
+        data_path = os.path.join(TRAIN_DATA, image_type)
+    elif image_type == "Test":
+        data_path = TEST_DATA
+    elif image_type == "AType_1" or \
+          image_type == "AType_2" or \
+          image_type == "AType_3":
+        data_path = os.path.join(ADDITIONAL_DATA, image_type)
+    else:
+        raise Exception("Image type '%s' is not recognized" % image_type)
+
+    ext = 'jpg'
+    return os.path.join(data_path, "{}.{}".format(image_id, ext))
+
+def get_image_data(image_id, image_type):
+    """
+    Method to get image data as np.array specifying image id and type
+    """
+    fname = get_filename(image_id, image_type)
+    img = cv2.imread(fname)
+    assert img is not None, "Failed to read image : %s, %s" % (image_id, image_type)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+    return img
+
+def maxHist(hist):
+    """
+    Algorithm for getting the maximum histogram of a given patch
+    """
+    maxArea = (0, 0, 0)
+    height = []
+    position = []
+    for i in range(len(hist)):
+        if (len(height) == 0):
+            if (hist[i] > 0):
+                height.append(hist[i])
+                position.append(i)
+        else:
+            if (hist[i] > height[-1]):
+                height.append(hist[i])
+                position.append(i)
+            elif (hist[i] < height[-1]):
+                while (height[-1] > hist[i]):
+                    maxHeight = height.pop()
+                    area = maxHeight * (i-position[-1])
+                    if (area > maxArea[0]):
+                        maxArea = (area, position[-1], i)
+                    last_position = position.pop()
+                    if (len(height) == 0):
+                        break
+                position.append(last_position)
+                if (len(height) == 0):
+                    height.append(hist[i])
+                elif(height[-1] < hist[i]):
+                    height.append(hist[i])
+                else:
+                    position.pop()
+    while (len(height) > 0):
+        maxHeight = height.pop()
+        last_position = position.pop()
+        area =  maxHeight * (len(hist) - last_position)
+        if (area > maxArea[0]):
+            maxArea = (area, len(hist), last_position)
+    return maxArea
+
+
+def maxRect(img):
+    """
+    Find the maximum connected rectangle region in an image
+    """
+    maxArea = (0, 0, 0)
+    addMat = np.zeros(img.shape)
+    for r in range(img.shape[0]):
+        if r == 0:
+            addMat[r] = img[r]
+            area = maxHist(addMat[r])
+            if area[0] > maxArea[0]:
+                maxArea = area + (r,)
+        else:
+            addMat[r] = img[r] + addMat[r-1]
+            addMat[r][img[r] == 0] *= 0
+            area = maxHist(addMat[r])
+            if area[0] > maxArea[0]:
+                maxArea = area + (r,)
+    return (int(maxArea[3]+1-maxArea[0]/abs(maxArea[1]-maxArea[2])), maxArea[2], maxArea[3], maxArea[1], maxArea[0])
+
+def cropCircle(img):
+    """
+    Cropping the circle often seen in cervigrams using maxRect and maxHist to detect
+    a maximum connected region
+    """
+    if(img.shape[0] > img.shape[1]):
+        tile_size = (int(img.shape[1]*256/img.shape[0]),256)
+    else:
+        tile_size = (256, int(img.shape[0]*256/img.shape[1]))
+
+    img = cv2.resize(img, dsize=tile_size)
+
+    gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY);
+    _, thresh = cv2.threshold(gray, 10, 255, cv2.THRESH_BINARY)
+
+    _, contours, _ = cv2.findContours(thresh.copy(),cv2.RETR_TREE,cv2.CHAIN_APPROX_NONE)
+
+    main_contour = sorted(contours, key = cv2.contourArea, reverse = True)[0]
+
+    ff = np.zeros((gray.shape[0],gray.shape[1]), 'uint8')
+    cv2.drawContours(ff, main_contour, -1, 1, 15)
+    ff_mask = np.zeros((gray.shape[0]+2,gray.shape[1]+2), 'uint8')
+    cv2.floodFill(ff, ff_mask, (int(gray.shape[1]/2), int(gray.shape[0]/2)), 1)
+    #cv2.circle(ff, (int(gray.shape[1]/2), int(gray.shape[0]/2)), 3, 3, -1)
+
+    rect = maxRect(ff)
+    img_crop = img[min(rect[0],rect[2]):max(rect[0],rect[2]), min(rect[1],rect[3]):max(rect[1],rect[3])]
+    cv2.rectangle(ff,(min(rect[1],rect[3]),min(rect[0],rect[2])),(max(rect[1],rect[3]),max(rect[0],rect[2])),3,2)
+
+    #plt.subplot(121)
+    #plt.imshow(img)
+    #plt.subplot(122)
+    #plt.imshow(ff)
+    #plt.show()
+
+    return img_crop
+def Ra_space(img, Ra_ratio, a_threshold):
+    """
+    Assigning each pixel in the image 2 values:
+    a = max(value of channel, a_threshold) (a_threshold = 150)
+    R = distance from center pixel
+    """
+    imgLab = cv2.cvtColor(img, cv2.COLOR_RGB2LAB);
+    w = img.shape[0]
+    h = img.shape[1]
+    Ra = np.zeros((w*h, 2))
+    for i in range(w):
+        for j in range(h):
+            R = math.sqrt((w/2-i)*(w/2-i) + (h/2-j)*(h/2-j))
+            Ra[i*h+j, 0] = R
+            Ra[i*h+j, 1] = min(imgLab[i][j][1], a_threshold)
+
+    Ra[:,0] /= max(Ra[:,0])
+    Ra[:,0] *= Ra_ratio
+    Ra[:,1] /= max(Ra[:,1])
+
+    return Ra
+
+def getData():
+    """
+    Return X (a numpy array of RGB images of segmented cervigrams with same
+    size 58x58), Y (the labels of the images), and the size of the images
+    """
+    k = 0
+    ids = [sorted(type_1_ids), sorted(type_2_ids), sorted(type_3_ids)]
+    X_train = []
+    Y_train = []
+    min_dim = 1000
+    while k < 3:
+        for image_id in ids[k]:
+            img = get_image_data(image_id, 'Type_%i' % (k+1))
+            img = cropCircle(img)
+            w = img.shape[0]
+            h = img.shape[1]
+            imgLab = cv2.cvtColor(img, cv2.COLOR_RGB2LAB);
+            # Saturating the a-channel at 150 helps avoiding wrong segmentation
+            # in the case of close-up cervix pictures where the bloody os is falsly segemented as the cervix.
+            Ra = Ra_space(img, 1.0, 150)
+            a_channel = np.reshape(Ra[:,1], (w,h))
+            plt.subplot(121)
+            plt.imshow(a_channel)
+
+            g = mixture.GaussianMixture(n_components = 2, covariance_type = 'diag', random_state = 0, init_params = 'kmeans')
+            image_array_sample = shuffle(Ra, random_state=0)[:1000]
+            g.fit(image_array_sample)
+            labels = g.predict(Ra)
+            labels += 1 # Add 1 to avoid labeling as 0 since regionprops ignores the 0-label.
+
+            # The cluster that has the highest a-mean is selected.
+            labels_2D = np.reshape(labels, (w,h))
+            gg_labels_regions = measure.regionprops(labels_2D, intensity_image = a_channel)
+            gg_intensity = [prop.mean_intensity for prop in gg_labels_regions]
+            cervix_cluster = gg_intensity.index(max(gg_intensity)) + 1
+
+            mask = np.zeros((w * h,1),'uint8')
+            mask[labels==cervix_cluster] = 255
+            mask_2D = np.reshape(mask, (w,h))
+
+            cc_labels = measure.label(mask_2D, background=0)
+            regions = measure.regionprops(cc_labels)
+            areas = [prop.area for prop in regions]
+
+            regions_label = [prop.label for prop in regions]
+            largestCC_label = regions_label[areas.index(max(areas))]
+            mask_largestCC = np.zeros((w,h),'uint8')
+            mask_largestCC[cc_labels==largestCC_label] = 255
+
+            img_masked = img.copy()
+            img_masked[mask_largestCC==0] = (0,0,0)
+            img_masked_gray = cv2.cvtColor(img_masked, cv2.COLOR_RGB2GRAY);
+
+            _,thresh_mask = cv2.threshold(img_masked_gray,0,255,0)
+
+            kernel = np.ones((11,11), np.uint8)
+            thresh_mask = cv2.dilate(thresh_mask, kernel, iterations = 1)
+            thresh_mask = cv2.erode(thresh_mask, kernel, iterations = 2)
+            _, contours_mask, _ = cv2.findContours(thresh_mask.copy(),cv2.RETR_TREE,cv2.CHAIN_APPROX_NONE)
+
+            main_contour = sorted(contours_mask, key = cv2.contourArea, reverse = True)[0]
+
+            x,y,w,h = cv2.boundingRect(main_contour)
+            cv2.rectangle(img,(x,y),(x+w,y+h),255,3)
+            img_gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+            sub_img = img_gray[y:y+h, x:x+w]
+            dim = max(h, w)
+            if dim < min_dim:
+                min_dim = dim
+            sub_img = cv2.resize(sub_img, (dim, dim))
+            X_train.append(sub_img)
+            Y_train.append(k+1)
+            #print "shape sub_img", sub_img.shape
+            #cv2.imshow("sub img", sub_img)
+            #cv2.waitKey(0)
+            #plt.subplot(122)
+            #plt.imshow(sub_img)
+            #plt.show()
+
+        k = k + 1;
+
+    # Resizing all the images to min_dim x min_dim
+    #print "all Y", Y_train
+    new_X_train = []
+    for img in X_train:
+        new_img = cv2.resize(img, (min_dim, min_dim))
+        new_X_train.append(new_img)
+        #cv2.imshow("train img", img)
+        #cv2.waitKey(0)
+
+    # Creating X a np array of all the rgb images
+    print "X_train original", len(new_X_train), new_X_train[0].shape
+    X = np.stack(tuple(new_X_train))
+
+
+    # - normalize each uint8 image to the value interval [0, 1] as float image
+    X = X * (2.0/255.0) - 1.0
+
+
+    # Use CNN with F = 5, K = 50, P = 0, S = 1
+    from sklearn.preprocessing import LabelEncoder
+    from sklearn.preprocessing import Normalizer
+
+
+    Y = LabelEncoder().fit_transform(Y_train).reshape(-1)
+    print "After getData X: ", X.shape, "Y:", Y.shape
+    return X, Y, min_dim
+
+
+def getSubmitData():
+    """
+    Returning X (an array of RGB images of segmented cervigrams with the same size)
+    and the size of the images
+    """
+    X_submit = []
+    min_dim = 1000
+    for image_id in test_ids:
+
+        img = get_image_data(image_id, 'Test')
+        img = cropCircle(img)
+        w = img.shape[0]
+        h = img.shape[1]
+
+        imgLab = cv2.cvtColor(img, cv2.COLOR_RGB2LAB);
+
+        # Saturating the a-channel at 150 helps avoiding wrong segmentation
+        # in the case of close-up cervix pictures where the bloody os is falsly segemented as the cervix.
+        Ra = Ra_space(img, 1.0, 150)
+        a_channel = np.reshape(Ra[:,1], (w,h))
+        plt.subplot(121)
+        plt.imshow(a_channel)
+
+        g = mixture.GaussianMixture(n_components = 2, covariance_type = 'diag', random_state = 0, init_params = 'kmeans')
+        image_array_sample = shuffle(Ra, random_state=0)[:1000]
+        g.fit(image_array_sample)
+        labels = g.predict(Ra)
+        labels += 1 # Add 1 to avoid labeling as 0 since regionprops ignores the 0-label.
+
+        # The cluster that has the highest a-mean is selected.
+        labels_2D = np.reshape(labels, (w,h))
+        gg_labels_regions = measure.regionprops(labels_2D, intensity_image = a_channel)
+        gg_intensity = [prop.mean_intensity for prop in gg_labels_regions]
+        cervix_cluster = gg_intensity.index(max(gg_intensity)) + 1
+
+        mask = np.zeros((w * h,1),'uint8')
+        mask[labels==cervix_cluster] = 255
+        mask_2D = np.reshape(mask, (w,h))
+
+        cc_labels = measure.label(mask_2D, background=0)
+        regions = measure.regionprops(cc_labels)
+        areas = [prop.area for prop in regions]
+
+        regions_label = [prop.label for prop in regions]
+        largestCC_label = regions_label[areas.index(max(areas))]
+        mask_largestCC = np.zeros((w,h),'uint8')
+        mask_largestCC[cc_labels==largestCC_label] = 255
+
+        img_masked = img.copy()
+        img_masked[mask_largestCC==0] = (0,0,0)
+        img_masked_gray = cv2.cvtColor(img_masked, cv2.COLOR_RGB2GRAY);
+
+        _,thresh_mask = cv2.threshold(img_masked_gray,0,255,0)
+
+        kernel = np.ones((11,11), np.uint8)
+        thresh_mask = cv2.dilate(thresh_mask, kernel, iterations = 1)
+        thresh_mask = cv2.erode(thresh_mask, kernel, iterations = 2)
+        _, contours_mask, _ = cv2.findContours(thresh_mask.copy(),cv2.RETR_TREE,cv2.CHAIN_APPROX_NONE)
+
+        main_contour = sorted(contours_mask, key = cv2.contourArea, reverse = True)[0]
+
+        x,y,w,h = cv2.boundingRect(main_contour)
+        cv2.rectangle(img,(x,y),(x+w,y+h),255,3)
+        sub_img = img_masked[y:y+h, x:x+w]
+        dim = max(h, w)
+        if dim < min_dim:
+            min_dim = dim
+        sub_img = cv2.resize(sub_img, (dim, dim))
+        X_submit.append(sub_img)
+
+    new_X_submit = []
+    for img in X_submit:
+        new_img = cv2.resize(img, (min_dim, min_dim))
+        new_X_submit.append(new_img)
+        #cv2.imshow("train img", img)
+        #cv2.waitKey(0)
+
+    # Creating X a np array of all the rgb images
+
+    X = np.stack(tuple(new_X_submit))
+
+
+    # - normalize each uint8 image to the value interval [0, 1] as float image
+    # - rgb to gray
+    # - downsample image to rescaled_dim X rescaled_dim
+    # - L2 norm of each sample = 1
+
+    classifier_input = X.reshape(-1, min_dim*min_dim).astype(np.float32)
+    X = X * (2.0/255.0) - 1.0
+    print "X (to be submitted)", X.shape
+    return X, min_dim
---
+++ code/testRuns.py
@@ -0,0 +1,276 @@
+from time import time
+import sys
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import cv2
+import math
+from glob import glob
+
+import os
+from numpy import sqrt, histogram
+np.random.seed(123)
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Activation, Flatten
+from keras.layers import Convolution2D, MaxPooling2D
+from keras.utils import np_utils
+from keras.utils import np_utils
+#from sklearn.multiclass import OneVsRestClassifer
+
+from preprocessing import *
+from sklearn.model_selection import train_test_split
+from keras.layers.normalization import BatchNormalization
+from keras.optimizers import Adam
+from keras import regularizers
+np.random.seed(123)  # for reproducibility
+
+from numpy.lib.stride_tricks import as_strided
+import scipy.cluster.vq as vq
+from sklearn.utils import shuffle
+
+### MAIN
+def main():
+    X, Y, min_dim = getData()
+    X, Y = shuffle(X, Y, random_state=0)
+    #X_submit, dim_submit = getSubmitData()
+    X_traintune, X_test, Y_traintune, Y_test = train_test_split(X, Y, test_size=0.33)
+    
+    CNNplot(X_traintune, Y_traintune, 32, 8, 0, min_dim) #1
+    X_train, X_tune, Y_train, Y_tune = train_test_split(X_traintune, Y_traintune, test_size=.2)
+    BOWS(X_train, Y_train, X_test, Y_test, 5, 400)
+    MLPR(X_train, Y_train, X_test, Y_test)
+    #CNNplot(X_traintune, Y_traintune, 16, 8, 0, min_dim)
+    #CNNplot(X_traintune, Y_traintune, 16, 8, 2, min_dim)
+    #CNNplot(X_traintune, Y_traintune, 16, 16, 0, min_dim) #2
+    #CNNplot(X_traintune, Y_traintune, 32, 8, 0, min_dim) #3
+    #CNNplot(X_traintune, Y_traintune, 32, 8, 1, min_dim) #4
+    #CNNplot(X_traintune, Y_traintune, 32, 8, 2, min_dim) #5
+    #CNNplot(X_traintune, Y_traintune, 64, 8, 3, min_dim) #8
+    #CNNplot(X_traintune, Y_traintune, 32, 8, 1, min_dim) #9 redo with F = 5
+    #CNNplot(X_traintune, Y_traintune, 32, 8, 2, min_dim) #10 redo with F = 11
+    #CNNplot(X_traintune, Y_traintune, 32, 8, 4, min_dim) #9 redo
+    #CNN(X_train, Y_train, X_test, Y_test, min_dim)
+    #MLPR(X_train, Y_train, X_test, Y_test)
+    #print "X_train", X_train.shape
+    #print "X_test", X_train.shape
+    #print "X_submit", X_submit.shape
+    print "===========================================\n"
+
+   
+def MLPR(X_train, Y_train, X_test, Y_test):
+    """
+    Our multi-layer perceptron method using a single layer with 16 nodes
+    """
+
+    X_train = X_train.reshape(len(X_train), -1)
+    X_test = X_test.reshape(len(X_test), -1)
+    from sklearn.neural_network import MLPClassifier
+    mlp = MLPClassifier(hidden_layer_sizes=(16), max_iter=50, alpha=1e-4,\
+        solver='adam', activation='relu', verbose=0, tol=1e-4, random_state=1)
+    start_time = time()
+    mlp.fit(X_train, Y_train)
+    print "time:", time()-start_time
+    print("\n Train set score %f\n" % mlp.score(X_train, Y_train))
+    print("\nTest set score: %f\n" % mlp.score(X_test, Y_test))
+
+
+
+def BOWS(X_train, Y_train, X_test, Y_test, P, K):
+    """
+    Our implementation for Bags of Visual Words
+    """
+    from sklearn.decomposition import PCA
+    from sklearn.preprocessing import normalize
+    # Getting the patches
+    patches, size = view_patches(X_train, P)
+    print "Patches", patches.shape
+    patches = normalize(patches)
+    pca = PCA(n_components=3, whiten=True)
+    pca.fit(patches)
+    # Getting the codebook
+    codebook, distortion = vq.kmeans(patches, K)
+    print "codebook", codebook.shape
+    ### Visualizing the codebook clustering
+    #assignment, cdist = vq.vq(patches, codebook)
+    #plt.scatter(patches[:, 0], patches[:,1], c=assignment)
+    #plt.show()
+    ########################
+    histTrain = computeHistogram(patches, size, codebook)
+    patchesTest, sizeTest = view_patches(X_test, P)
+    patchesTest = normalize(patchesTest)
+    pca.fit(patchesTest)
+    assert(size == sizeTest)
+    histTest = computeHistogram(patchesTest, sizeTest, codebook)
+
+    from sklearn.ensemble import AdaBoostClassifier
+    from sklearn.tree import DecisionTreeClassifier
+    bdt_discrete = AdaBoostClassifier(DecisionTreeClassifier(max_depth=5),\
+        n_estimators=600, learning_rate=0.01, algorithm="SAMME")
+    start_time = time()
+    bdt_discrete.fit(histTrain, Y_train)
+    t = time()-start_time
+    print("\nTrain set score: %f\n" %bdt_discrete.score(histTrain, Y_train))
+    print "Time fit:", t
+    print("\nTest set score: %f\n" %bdt_discrete.score(histTest, Y_test))
+
+def computeHistogram(patches, size, codebook):
+    """
+    Computing histogram for each image based on the codebook
+    """
+    print "Computing histograms ...."
+    # Compute histograms
+    #descriptors = np.arange(len(X_train))
+    histograms = []
+    for i in range(0, len(patches), size):
+        #### need to get patches here as well
+        p = patches[i:i+size]
+        code, dist = vq.vq(p, codebook)
+        #plt.scatter(p[:,0], p[:,1], c=code)
+        #plt.show()
+        histograms_of_words, bin_edges = histogram(code, bins=range(codebook.shape[0] + 1), normed=True)
+        #plt.hist(code, bins=range(codebook.shape[0] + 1), normed=True)
+        #plt.title("Histogram of visual words")
+        #plt.show()
+        histograms.append(histograms_of_words)
+    histograms = np.array(histograms)
+    print "histograms", histograms.shape
+    return histograms
+def view_patches(images, patch_size):
+    """
+    Getting patches of a specific size from an array of images
+    """
+    print "Getting patches..."
+    n, h, w = images.shape
+    esize = images.itemsize
+
+    nx = w-patch_size+1
+    ny = h-patch_size+1
+
+    patches = as_strided(images, shape=[n,ny,nx,patch_size,patch_size],
+                         strides=[h*w*esize, w*esize, esize, w*esize, esize])
+    print "Shape of patches prereshape", patches.shape
+    p = np.reshape(patches, (n*ny*nx, patch_size*patch_size))
+    #patches = np.reshape(n*nx*ny, patch_size*patch_size)
+
+    return p, ny*nx
+def CNNplot(X, Y, K, D, option, min_dim):
+    X = X.reshape(X.shape[0], min_dim, min_dim, 1)
+    Y = np_utils.to_categorical(Y, 3)
+    model = Sequential()
+    if option == 1:
+        F = 5
+        B = 32
+    if option == 2:
+        F = 11
+        B = 32
+    if option == 0:
+        F = 3
+        B = 10
+    if option == 4:
+        F = 3
+        B = 32
+    model.add(Convolution2D(K, (F, F),kernel_initializer='random_normal',
+     activation='relu', input_shape=(min_dim,min_dim, 1)))
+    model.add(MaxPooling2D(pool_size=(2,2)))
+
+    model.add(BatchNormalization())
+    if option >=1:
+        model.add(Dropout(0.25))
+    model.add(Flatten())
+    model.add(Dense(D, activation='relu',kernel_regularizer=regularizers.l2(0.01),\
+activity_regularizer=regularizers.l1(0.01)))
+    model.add(BatchNormalization())
+
+    model.add(Dropout(0.25))
+    model.add(Dense(3, activation='softmax'))
+
+    #  Compile model
+    model.compile(loss='categorical_crossentropy',
+                  optimizer='adam',
+                  metrics=['accuracy'])
+
+
+    #  Fit model on training data
+    t0 = time()
+    history = model.fit(X, Y, validation_split=0.2,
+              batch_size=B, nb_epoch=8, verbose=1)
+    print "Time: ", time()-t0
+    print(history.history.keys())
+    # summarize history for accuracy
+    plt.plot(history.history['acc'])
+    plt.plot(history.history['val_acc'])
+    plt.title('model accuracy')
+    plt.ylabel('accuracy')
+    plt.xlabel('epoch')
+    plt.legend(['train', 'test'], loc='upper left')
+    plt.show()
+    # summarize history for loss
+    plt.plot(history.history['loss'])
+    plt.plot(history.history['val_loss'])
+    plt.title('model loss')
+    plt.ylabel('loss')
+    plt.xlabel('epoch')
+    plt.legend(['train', 'test'], loc='upper left')
+    plt.show()
+    print "________________________________________"
+
+
+    # Getting the weights of the first Convolution layer to display
+    """
+    conv_layer = model.layers[1]
+    kernels = conv_layer.get_weights()[0]
+    print "kernels array has shape", kernels.shape
+    kernel_h, kernel_w, num_input_channels, num_kernels = kernels.shape
+
+    # Parameters for display
+    KERNELS_PER_ROW = 8 # We will display an image with 8 kernels per row
+    MARGIN = 2 # Spacing between kernels
+    ZOOM = 8 # Magnification factor for kernels
+
+    # Determine # of rows
+    num_rows = int(np.ceil(float(num_kernels) / KERNELS_PER_ROW))
+
+    # Determine dimensions of display
+    h = (kernel_h + MARGIN)*num_rows + MARGIN
+    w = (kernel_w + MARGIN)*KERNELS_PER_ROW + MARGIN
+
+    # Allocate space for display
+    display = 0.9*np.ones((h, w), dtype=kernels.dtype)
+
+    # Get kernel min/max for rescaling below
+    kmin = kernels.min()
+    kmax = kernels.max()
+
+    # For each kernel
+    for i in range(num_kernels):
+
+        # Find out row/column in display image
+        row = i / KERNELS_PER_ROW
+        col = i % KERNELS_PER_ROW
+
+        # Get pixel coordinates
+        y0 = row*(kernel_h + MARGIN) + MARGIN
+        x0 = col*(kernel_w + MARGIN) + MARGIN
+
+        # Get the i'th kernel
+        ki = kernels[:,:,0,i]
+
+        # Rescale to [0,1] interval
+        ki = (ki - kmin) / (kmax - kmin)
+
+        # Blit it into the display image
+        display[y0:y0+kernel_h, x0:x0+kernel_w] = ki
+
+    # Magnify the display image
+    display = cv2.resize(display, (ZOOM*w, ZOOM*h),
+                         interpolation=cv2.INTER_NEAREST)
+
+    # Show it
+    cv2.imshow('Kernels', display)
+    cv2.waitKey(0)
+    """
+
+
+if __name__=='__main__':
+    main()
+sys.exit()
